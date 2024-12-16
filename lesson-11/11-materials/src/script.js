cbd45a824370f4b9fa8befc9db1399d62d9e4df7
Elijah Won <138832737+ejinsw@users.noreply.github.com>
2024-12-16 00:13:54 -0600

    Complete lesson 11

---
+++ lesson-11/11-materials/src/script.js
@@ -0,0 +1,260 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import GUI from 'lil-gui'
+import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js'
+
+/**
+ * Debug
+ */
+const gui = new GUI()
+
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Textures
+ */
+const loadingManager = new THREE.LoadingManager()
+
+loadingManager.onProgress = () => {
+    console.log("Loading...")
+}
+
+const textureLoader = new THREE.TextureLoader(loadingManager)
+
+const alphaTexture = textureLoader.load('./textures/door/alpha.jpg')
+const ambientOcclusionTexture = textureLoader.load('./textures/door/ambientOcclusion.jpg')
+const colorTexture = textureLoader.load('./textures/door/color.jpg')
+const heightTexture = textureLoader.load('./textures/door/height.jpg')
+const metalnessTexture = textureLoader.load('./textures/door/metalness.jpg')
+const normalTexture = textureLoader.load('./textures/door/normal.jpg')
+const roughnessTexture = textureLoader.load('./textures/door/roughness.jpg')
+
+const matcapTexture = textureLoader.load('./textures/matcaps/3.png')
+
+const gradientTexture = textureLoader.load('./textures/gradients/3.jpg')
+
+colorTexture.colorSpace = THREE.SRGBColorSpace
+matcapTexture.colorSpace = THREE.SRGBColorSpace
+
+/**
+ * Objects
+ */
+// // MeshBasicMaterial
+// const material = new THREE.MeshBasicMaterial()
+// material.map = colorTexture
+// material.transparent = true
+// material.alphaMap = alphaTexture
+// material.side = THREE.DoubleSide
+
+// // MeshNormalMaterial
+// const material = new THREE.MeshNormalMaterial()
+// material.flatShading = true
+
+// // MeshMatcapMaterial
+// const material = new THREE.MeshMatcapMaterial()
+// material.matcap = matcapTexture
+
+// // MeshDepthMaterial
+// const material = new THREE.MeshDepthMaterial()
+
+// // MeshLambertMaterial
+// const material = new THREE.MeshLambertMaterial()
+
+// // MeshPhongMaterial
+// const material = new THREE.MeshPhongMaterial()
+// material.shininess = 100
+// material.specular = new THREE.Color(0x1188ff)
+
+// // MeshToonMaterial
+// const material = new THREE.MeshToonMaterial()
+// gradientTexture.minFilter = THREE.NearestFilter
+// gradientTexture.magFilter = THREE.NearestFilter
+// gradientTexture.generateMipmaps = false
+// material.gradientMap = gradientTexture
+
+// // MeshStandardMateral
+// const material = new THREE.MeshStandardMaterial()
+// material.metalness = 1
+// material.roughness = 1
+
+// material.map = colorTexture
+// material.aoMap = ambientOcclusionTexture
+// material.displacementMap = heightTexture
+// material.displacementScale = 0.05
+// material.metalnessMap = metalnessTexture
+// material.roughnessMap = roughnessTexture
+// material.normalMap = normalTexture
+// material.transparent = true
+// material.alphaMap = alphaTexture
+
+// gui.add(material, 'metalness').min(0).max(1).step(0.01)
+// gui.add(material, 'roughness').min(0).max(1).step(0.01)
+
+// MeshPhysicalMateral
+const material = new THREE.MeshPhysicalMaterial()
+material.metalness = 0
+material.roughness = 0
+
+// material.map = colorTexture
+// material.aoMap = ambientOcclusionTexture
+// material.displacementMap = heightTexture
+// material.displacementScale = 0.05
+// material.metalnessMap = metalnessTexture
+// material.roughnessMap = roughnessTexture
+// material.normalMap = normalTexture
+// material.transparent = true
+// material.alphaMap = alphaTexture
+
+// material.clearcoat = 1
+// material.clearcoatRoughness = 0
+
+// material.sheen = 1
+// material.sheenRoughness = 0.25
+// material.sheenColor.set(1, 1, 1)
+
+// material.iridescence = 1
+// material.iridescenceIOR = 1
+// material.iridescenceThicknessRange = [100, 800]
+
+material.transmission = 1
+material.ior = 1.5
+material.thickness = 0.5
+
+// gui.add(material, 'metalness').min(0).max(1).step(0.01)
+// gui.add(material, 'roughness').min(0).max(1).step(0.01)
+
+// gui.add(material, 'clearcoat').min(0).max(1).step(0.01)
+// gui.add(material, 'clearcoatRoughness').min(0).max(1).step(0.01)
+
+// gui.add(material, 'sheen').min(0).max(1).step(0.01)
+// gui.add(material, 'sheenRoughness').min(0).max(1).step(0.01)
+// gui.addColor(material, 'sheenColor')
+
+// gui.add(material, 'iridescence').min(0).max(1).step(0.001)
+// gui.add(material, 'iridescenceIOR').min(1).max(2.333).step(0.001)
+// gui.add(material.iridescenceThicknessRange, '0').min(1).max(1000).step(1)
+// gui.add(material.iridescenceThicknessRange, '1').min(1).max(1000).step(1)
+
+gui.add(material, 'transmission').min(0).max(1).step(0.01)
+gui.add(material, 'ior').min(0).max(10).step(0.01)
+gui.add(material, 'thickness').min(0).max(1).step(0.01)
+
+const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 64, 64),
+    material
+)
+sphere.position.x = -1.5
+
+const torus = new THREE.Mesh(
+    new THREE.TorusGeometry(0.3, 0.2, 64, 128),
+    material
+)
+torus.position.x = 1.5
+
+const plane = new THREE.Mesh(
+    new THREE.PlaneGeometry(1, 1, 100, 100),
+    material
+)
+
+scene.add(sphere, plane, torus)
+
+/**
+ * Lights
+ */
+// const ambientLight = new THREE.AmbientLight(0xffffff, 1)
+// scene.add(ambientLight)
+
+// const pointLight = new THREE.PointLight(0xffffff, 30)
+// pointLight.position.set(2,3,4)
+// scene.add(pointLight)
+
+/**
+ * Environment Map
+ */
+const rgbeloader = new RGBELoader(loadingManager)
+rgbeloader.load('./textures/environmentMap/2k.hdr', (environmentMap) => {
+    environmentMap.mapping = THREE.EquirectangularReflectionMapping
+    scene.background = environmentMap
+    scene.environment = environmentMap
+})
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () => {
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 1
+camera.position.y = 1
+camera.position.z = 2
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () => {
+    const elapsedTime = clock.getElapsedTime()
+
+    sphere.rotation.x = 0.1 * elapsedTime
+    sphere.rotation.y = 0.1 * elapsedTime
+
+    plane.rotation.x = 0.1 * elapsedTime
+    plane.rotation.y = 0.1 * elapsedTime
+
+    torus.rotation.x = 0.1 * elapsedTime
+    torus.rotation.y = 0.1 * elapsedTime
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
